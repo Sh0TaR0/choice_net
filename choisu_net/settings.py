fa04b53818d6cd5dc246014956f6b35bef27de48
Sho <sho045kobayashi@gmail.com>
2019-10-20 18:16:30 +0900

    will try to deploy

--- choisu_net/settings.py
+++ choisu_net/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 if not DEBUG:
     SECRET_KEY = os.environ['SECRET_KEY']
-    import django_heroku
+
     django_heroku.settings(locals())
 
 # SECURITY WARNING: don't run with debug turned on in production!
