6dd615e7c323e9202648e8e2abbb1ca7a8ffe01f
Sho <sho045kobayashi@gmail.com>
2019-10-21 12:36:27 +0900

    debug true

--- choisu_net/settings.py
+++ choisu_net/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=False)
+DEBUG = True
+# DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
