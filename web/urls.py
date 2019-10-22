6e667693aec957850dc1f827789d998fb5b95812
Sho <sho045kobayashi@gmail.com>
2019-10-22 23:19:02 +0900

    made some change to base.html

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sample/', views.sample, name='sample'),
 ]
--- web/views.py
+++ web/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
-
-
-def sample(request):
-    return render(request, 'sample.html')
